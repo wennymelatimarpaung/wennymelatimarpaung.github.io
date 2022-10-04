685731ca7e0ca0868a439bdc0161ba32506de8bb
wennymelatimarpaung <102161094+wennymelatimarpaung@users.noreply.github.com>
2022-10-04 21:46:04 +0800

    Posttest3

---
+++ Posttest3/javascripst.js
@@ -0,0 +1,21 @@
+alert("Selamat datang di Pizza Wenny");
+
+
+let searchbtn = document.querySelector('#search');
+    let searchfrm = document.querySelector('.head .search-bar');
+
+    searchbtn.onclick = () => {
+        searchfrm.classList.toggle('active');
+        menu.classList.remove('active');
+    }
+
+    let menu = document.querySelector('.head .navbar');
+    document.querySelector('#bars').onclick = () => {
+        menu.classList.toggle('active');
+        searchfrm.classList.remove('active');
+
+    }
+    window.onscroll = () => {
+        menu.classList.remove('active');
+        searchfrm.classList.remove('active');
+    }
